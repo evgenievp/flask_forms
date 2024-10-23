d782b6e257ec353013c00cf0b127f062513ac4ec
petar <evgenievp@yahoo.com>
2024-10-23 13:56:26 +0300

    Flask forms

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.secret_key = 'test'
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='email', validators=[DataRequired])
+    password = PasswordField(label='password', validators=[DataRequired])
+    submit = SubmitField(label='Log In')
+
+
+@app.route('/login')
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        return render_template('success.html')
+    else:
+        return render_template('denied.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
